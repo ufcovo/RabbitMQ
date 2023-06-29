f0ffa1c0af60030d6e850bb5acd63a6b8b08d2c5
Mert <79603812+ufcovo@users.noreply.github.com>
2023-06-29 14:46:04 +0300

    Initial commit

---
+++ RabbitMQ.Publisher/Program.cs
@@ -0,0 +1,28 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace RabbitMQ.Publisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://pmltsnbj:U2ZypivPsw_hV_F1nYD2U4IJ4QRNLgSd@shark.rmq.cloudamqp.com/pmltsnbj");
+
+            using var connection = factory.CreateConnection();
+
+            var channel = connection.CreateModel();
+            channel.QueueDeclare("hello-queue", true, false, false);
+
+            string message = "hello world";
+            var messageBody = Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish(string.Empty, "hello-queue", null, messageBody);
+
+            Console.WriteLine("Message is sended.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RabbitMQ.Subscriber/Program.cs
@@ -0,0 +1,33 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace RabbitMQ.Subscriber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://pmltsnbj:U2ZypivPsw_hV_F1nYD2U4IJ4QRNLgSd@shark.rmq.cloudamqp.com/pmltsnbj");
+
+            using var connection = factory.CreateConnection();
+
+            var channel = connection.CreateModel();
+            //channel.QueueDeclare("hello-queue", true, false, false);
+
+            var consumer = new EventingBasicConsumer(channel);
+
+            channel.BasicConsume("hello-queue", true, consumer);
+            consumer.Received += (object? sender, BasicDeliverEventArgs e) => {
+                var message = Encoding.UTF8.GetString(e.Body.ToArray());
+                Console.WriteLine("Coming message: " + message);
+            };
+
+
+            
+            Console.ReadLine();
+        }
+    }
+}
