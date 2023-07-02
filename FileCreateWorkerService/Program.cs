0519407f31d5711b9e86a0d22e77562ad4e378dd
Mert <79603812+ufcovo@users.noreply.github.com>
2023-07-02 11:52:59 +0300

    Database created (DB First)

--- FileCreateWorkerService/Program.cs
+++ FileCreateWorkerService/Program.cs
@@ -1,10 +1,15 @@
 using FileCreateWorkerService;
+using FileCreateWorkerService.Models;
 using FileCreateWorkerService.Services;
+using Microsoft.EntityFrameworkCore;
 using RabbitMQ.Client;
 
 IHost host = Host.CreateDefaultBuilder(args)
     .ConfigureServices((context, services) =>
     {
+        services.AddDbContext<AdventureWorks2019Context>(options =>
+            options.UseSqlServer(context.Configuration.GetConnectionString("SqlServer"))
+        );
         services.AddHostedService<Worker>();
         services.AddSingleton(sp =>
             new ConnectionFactory() 
